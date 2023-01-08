a500e157ddd5264f26d6481f879d4999d3c02fd1
Rafeekmh1 <120737751+Rafeekmh1@users.noreply.github.com>
2023-01-08 18:08:29 +0200

    Delete djangotestproj directory

--- djangotestproj/accounts/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import *
-
-admin.site.register(Doctor)
-admin.site.register(Patient)
-admin.site.register(Nurse)
-admin.site.register(BloodTest)
