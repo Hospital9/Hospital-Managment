0478d947683b16182819ae738cf87bdab42f957e
Rafeekmh1 <120737751+Rafeekmh1@users.noreply.github.com>
2023-01-17 14:16:54 +0200

    update_Tests_Rafeek

--- App/Auth/tests/test_authentication.py
+++ App/Auth/tests/test_authentication.py
@@ -1,5 +1,9 @@
-from django.test import TestCase 
-from django.urls import reverse 
+from django.test import TestCase
+from django.urls import reverse
+
+from django.contrib.auth.models import User
+from datetime import date
+from Doctors.models import Profile, Appointment
 
 class BaseTest(TestCase):
     def setUp(self): 
@@ -19,5 +23,33 @@ class RegisterTest(BaseTest):
         response = self.client.get(self.register_url)
         self.assertEqual(response.status_code,200)
         self.assertTemplateUsed(response,'Register/register.html')
-    
- 
+
+
+
+class DoctorDashboardViewTestCase(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='testuser', password='testpassword')
+        self.user.save()
+        self.profile = Profile.objects.create(user=self.user, is_doctor=True)
+        self.appointment = Appointment.objects.create(doctor=self.user.profile.doctor, date=date.today(),timeslot=2)
+
+    def test_doctor_dashboard_view_with_user_not_logged_in(self):
+        response = self.client.get(reverse('doctorDashboard'))
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, '/login/?next=/dashboard')
+
+    def test_doctor_dashboard_view_with_not_doctor_user(self):
+            self.client.login(username='testuser',password='testpassword')
+            self.profile.is_doctor = False
+            self.profile.save()
+            response = self.client.get(reverse('doctorDashboard'))
+            self.assertEqual(response.status_code, 200)
+            self.assertContains(response, "Please go back,this page it's only for staff.")
+
+    def test_doctor_dashboard_view_with_doctor_user(self):
+        self.client.login(username='testuser', password='testpassword')
+        response = self.client.get(reverse('doctorDashboard'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'Dashboard/doctorDashboard.html')
+        self.assertContains(response, 1)
+        self.assertContains(response, 1)
